45ca8f8113e72dc034223c8c9d238bce7b5ea034
김태영 <51165061+xodud001@users.noreply.github.com>
2021-10-05 22:59:10 +0900

    feat : 아이템 3.
    정적 팩토리 방식의 싱글톤

--- src/chapter2/item3/Singleton.java
+++ src/chapter2/item3/Singleton.java
@@ -1,9 +1,10 @@
 package chapter2.item3;
 
 public class Singleton {
-    // private 생성자는 Singleton.INSTANCE를 초기화할 때 딱 한 번만 호출
-    // 다른 생성자는 없기 때문에 전체 시스템에서 하나뿐임을 보장
-    public static final Singleton INSTANCE = new Singleton();
-
+    // Singleton.getInstance는 항상 같은 객체의 참조를 반환. 제 2의 Singleton 인스턴스란 만들어지지 않음
+    private static final Singleton INSTANCE = new Singleton();
     private Singleton(){}
+    public static Singleton getInstance() {
+        return INSTANCE;
+    }
 }
