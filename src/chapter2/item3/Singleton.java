6344d5be9c842f63946dbdd9ddd05616d10bed20
김태영 <51165061+xodud001@users.noreply.github.com>
2021-10-05 23:02:14 +0900

    feat : 아이템 3.
    열거 타입 방식의 싱글턴

--- src/chapter2/item3/Singleton.java
+++ src/chapter2/item3/Singleton.java
@@ -1,10 +1,11 @@
 package chapter2.item3;
 
-public class Singleton {
-    // Singleton.getInstance는 항상 같은 객체의 참조를 반환. 제 2의 Singleton 인스턴스란 만들어지지 않음
-    private static final Singleton INSTANCE = new Singleton();
-    private Singleton(){}
-    public static Singleton getInstance() {
-        return INSTANCE;
+public enum Singleton {
+    // 아주 복잡한 직렬화 상황이나 리플렉션 공격에서도 제2의 인스턴스가 생기는 일을 완벽히 막아준다.
+    // 대부분 상황에서는 원소가 하나뿐인 열거 타입이 싱글턴을 만드는 가장 좋은 방법이다.
+    INSTANCE;
+    
+    public void leaveTheBuilding() {
+        // Do Something
     }
 }
