2d2bdcb3cecbaf7493ea26d391e184c581e0f1ea
김태영 <51165061+xodud001@users.noreply.github.com>
2021-10-07 16:29:07 +0900

    feat : 아이템 5
    정적 유틸리티를 잘못 사용한 예

--- src/chapter2/item5/SpellChecker.java
+++ src/chapter2/item5/SpellChecker.java
@@ -3,12 +3,13 @@ package chapter2.item5;
 import java.util.ArrayList;
 import java.util.List;
 
-// 정적 유틸리티를 잘못 사용한 예 - 유연하지 않고 테스트하기 어렵다.
+// 싱글턴을 잘못 사용한 예 - 유연하지 않고 테스트하기 어렵다.
 public class SpellChecker {
-    private static final Lexicon dictionary = new Lexicon();
+    private final Lexicon dictionary = new Lexicon();
 
     private SpellChecker(){}
-
+    public static SpellChecker INSTANCE = new SpellChecker();
+    
     public static boolean isValid(String word){ return true; }
     public static List<String> suggestions(String typo){ return new ArrayList<String>();}
 }
