f43581c4f60a3f1912dcbb975d576c12c79b4531
김태영 <51165061+xodud001@users.noreply.github.com>
2021-10-07 16:32:00 +0900

    feat : 아이템 5
    의존 객체 주입을 통해 유연성을 높이고 테스트를 용이하게 함

--- src/chapter2/item5/SpellChecker.java
+++ src/chapter2/item5/SpellChecker.java
@@ -2,14 +2,16 @@ package chapter2.item5;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-// 싱글턴을 잘못 사용한 예 - 유연하지 않고 테스트하기 어렵다.
+// 의존 객체 주입을 통해 유연성을 높이고 테스트를 용이하게 해준다.
 public class SpellChecker {
-    private final Lexicon dictionary = new Lexicon();
+    private final Lexicon dictionary;
 
-    private SpellChecker(){}
-    public static SpellChecker INSTANCE = new SpellChecker();
-    
+    public SpellChecker(Lexicon dictionary){
+        this.dictionary = Objects.requireNonNull(dictionary);
+    }
+
     public static boolean isValid(String word){ return true; }
     public static List<String> suggestions(String typo){ return new ArrayList<String>();}
 }
