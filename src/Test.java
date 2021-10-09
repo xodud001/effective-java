6963da83efd075c2557bb877504bb3bb549b8fd5
김태영 <51165061+xodud001@users.noreply.github.com>
2021-10-09 20:04:50 +0900

    test : primitive 타입과 wrapper 타입의 연산 추적

--- src/Test.java
+++ src/Test.java
@@ -3,16 +3,17 @@ import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
-        map.put("KTY", "김태영");
-        Set<String> keySet1 = map.keySet();
-        map.put("KDY", "김동영");
-        Set<String> keySet2 = map.keySet();
+        long start = System.currentTimeMillis();
 
-        keySet1.forEach(System.out::println);
-        keySet2.forEach(System.out::println);
+        Long sum = 0L;
+//        Long pre = sum;
+//
+//        sum += 1;
 
-        System.out.println(keySet1.equals(keySet2));
 
+        for (long i = 0L; i < Integer.MAX_VALUE; i++)
+            sum += i;
+
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
