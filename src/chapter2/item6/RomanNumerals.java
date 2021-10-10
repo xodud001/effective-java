a8d20960d4bbc4d37fecf39fe8301af062083c26
김태영 <51165061+xodud001@users.noreply.github.com>
2021-10-10 21:01:44 +0900

    feat : 아이템 6 불필요한 객체 생성을 피하라
    Pattern의 반복적인 생성을 막기 위해 Pattern 인스턴스를 직접 캐싱해두고 사용했을 때 실행 시간이 줄어 드는지 테스트

--- src/chapter2/item6/RomanNumerals.java
+++ src/chapter2/item6/RomanNumerals.java
@@ -1,11 +1,14 @@
 package chapter2.item6;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class RomanNumerals {
-    static  boolean isRomanNumeral(String s){
-        return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
+    private static final Pattern ROMAN = Pattern.compile("^(?=.)M*(C[MD]|D?C{0,3})"
             + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+
+    static boolean isRomanNumeral(String s){
+        return ROMAN.matcher(s).matches();
     }
 
     public static void main(String[] args) {
