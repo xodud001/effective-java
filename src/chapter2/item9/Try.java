ed717f593927f1600d04ce1a6335cc8e1135afef
김태영 <51165061+xodud001@users.noreply.github.com>
2021-10-14 21:21:36 +0900

    feat : 아이템 9. try-finally 보다는 try-with-resources를 사용해라
    자원이 2개 이상일 경우 try-finally는 코드가 지저분해진다.

--- src/chapter2/item9/Try.java
+++ src/chapter2/item9/Try.java
@@ -4,8 +4,9 @@ import java.io.*;
 
 public class Try {
     public static void main(String[] args) throws IOException {
-        String firstLine = firstLineOfFile("test.txt");
-        System.out.println(firstLine);
+        copy("test.txt", "dst.txt");
+        String result = firstLineOfFile("dst.txt");
+        System.out.println(result);
     }
 
     public static String firstLineOfFile(String path) throws IOException {
@@ -15,5 +16,23 @@ public class Try {
         }finally {
             br.close();
         }
+    }
+
+    public static void copy(String src, String dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        try{
+            OutputStream out = new FileOutputStream(dst);
+            try{
+                byte[] buf = new byte[255];
+                int n;
+                while( (n = in.read(buf)) >= 0)
+                    out.write(buf, 0, n);
+            }finally {
+                out.close();
+            }
+        }finally {
+            in.close();
+        }
+
     }
 }
