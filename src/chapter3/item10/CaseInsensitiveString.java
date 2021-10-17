e463b1c2dd8f6ddd8971d52d67e4e5169c42ee40
김태영 <51165061+xodud001@users.noreply.github.com>
2021-10-17 14:53:33 +0900

    아이템 10. equals는 일반 규약을 지켜 재정의하라
    대칭성을 위배한 equals() 수정으로 대칭성을 준수

--- src/chapter3/item10/CaseInsensitiveString.java
+++ src/chapter3/item10/CaseInsensitiveString.java
@@ -1,5 +1,8 @@
 package chapter3.item10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class CaseInsensitiveString {
     private final String s;
 
@@ -10,11 +13,9 @@ public final class CaseInsensitiveString {
 
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof CaseInsensitiveString)
-            return s.equalsIgnoreCase( ((CaseInsensitiveString) obj).s);
-        if(obj instanceof String)
-            return s.equalsIgnoreCase((String)obj);
-        return false;
+        //CaseInsentiveString의 equals를 Stirng과 연동하겠다는 생각은 버려야 한다.
+        return obj instanceof CaseInsensitiveString &&
+                ((CaseInsensitiveString)obj).s.equalsIgnoreCase(s);
     }
 
     public static void main(String[] args) {
@@ -25,5 +26,11 @@ public final class CaseInsensitiveString {
         // 하지만 cis.equals(s)는 true인데 s.equals(cis)가 false이기 때문에 대칭성을 위반했다.
         System.out.println(cis.equals(s));
         System.out.println(s.equals(cis));
+
+        List<CaseInsensitiveString> list = new ArrayList<>();
+        list.add(cis);
+        // 현재 JDK에서는 false를 반환하지만 다른 버전에서는 어떻게 동작될지 모름.
+        // equals 규약을 어기면 그 객체를 사용하는 다른 객체들이 어떻게 반응할지 할 수 없다.
+        System.out.println(list.contains(s));
     }
 }
