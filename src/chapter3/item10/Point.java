10c061d81b581b6323fff4e1899abbd497ac5470
김태영 <51165061+xodud001@users.noreply.github.com>
2021-10-19 21:14:12 +0900

    아이템 10. equals는 일반 규약을 지켜 재정의하라
    리스코프 치환 원칙을 위배한 경우 작성한 코드가 원하는 대로 동작하지 않음

--- src/chapter3/item10/Point.java
+++ src/chapter3/item10/Point.java
@@ -1,5 +1,7 @@
 package chapter3.item10;
 
+import java.util.Set;
+
 public class Point {
     private final int x;
     private final int y;
@@ -23,6 +25,10 @@ public class Point {
     }
 
     public static void main(String[] args) {
+        test2();
+    }
+
+    public static void test1(){
         // x.equals(y) == true and y.equals(z) == true 이면
         // x.equals(z)도 true여야 하지만 false가 나옴으로써 추이성이 위배된다.
         ColorPoint p1 = new ColorPoint(1, 2, Color.RED);
@@ -33,5 +39,20 @@ public class Point {
         System.out.println(p2.equals(p3));
 
         System.out.println(p1.equals(p3));
+    }
+
+    public static void test2(){
+        // CounterPoint는 Point를 상속 받았기 때문에
+        // 어디서든지 Point로 동작해야 하지만 Point의 equals()를
+        // 리스코프 치환 원칙을 위배해서 정의했기 때문에 제대로 된 동작을 하지 않는다.
+        CounterPoint cp = new CounterPoint(1, 0);
+        System.out.println(unitCircle.contains(cp));
+    }
+    private static final Set<Point> unitCircle = Set.of(
+            new Point(1, 0), new Point(0, 1),
+            new Point(-1, 0), new Point(0, -1)
+    );
+    public static boolean onUnitCircle(Point p) {
+        return unitCircle.contains(p);
     }
 }
