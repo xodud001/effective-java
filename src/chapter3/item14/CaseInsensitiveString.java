62efff7fb563ae190b3922128052ce945a4fa0ea
김태영 <51165061+xodud001@users.noreply.github.com>
2021-10-24 23:26:24 +0900

    feat : item 14 Comparable을 구현할지 고려하라
    제공되는 compare 메소드를 이용한 구현

---
+++ src/chapter3/item14/CaseInsensitiveString.java
@@ -0,0 +1,10 @@
+package chapter3.item14;
+
+public class CaseInsensitiveString implements Comparable<CaseInsensitiveString> {
+    private String s;
+    
+    @Override
+    public int compareTo(CaseInsensitiveString cis) {
+        return String.CASE_INSENSITIVE_ORDER.compare(s, cis.s);
+    }
+}
