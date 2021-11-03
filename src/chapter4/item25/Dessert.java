47b87e2b98c28e91f701dd00e4d30ad978f63877
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-03 21:25:04 +0900

    feat : item 25 톱레벨 클래스는 한 파일에 하나만 담으라
    첫 번째 파일애 2개의 클래스를 담고 두 번째 파일에 다시 2개의 크래스를 담으면 javac로 컴파일 할 때 컴파일러에 어느 소스 파일을 먼저 건네느냐에 따라 동작이 달라진다.

---
+++ src/chapter4/item25/Dessert.java
@@ -0,0 +1,9 @@
+package chapter4.item25;
+
+class Utensil {
+    static final String NAME = "pot";
+}
+
+class Dessert{
+    static final String NAME = "pie";
+}
---
+++ src/chapter4/item25/Utensil.java
@@ -0,0 +1,9 @@
+package chapter4.item25;
+
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert{
+    static final String NAME = "cake";
+}
