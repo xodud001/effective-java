6c3adc49bd28bfd40cf886ea0d83f94479336658
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-04 23:37:25 +0900

    feat : item 26 Raw Type은 사용하지 말라
    parameterized type으로 정의해서 타입 안정성을 확보와 잠재적인 런타임 시에 발생할 수 있는 예외를 없앴다.

--- src/chapter5/item26/Test.java
+++ src/chapter5/item26/Test.java
@@ -2,22 +2,17 @@ package chapter5.item26;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 
 public class Test {
-    // 컬렉션의 Raw type. 절대 사용해서는 안된다.
-    // 자바 5 이전에 지원하던 코드들의 호환성을 위해 아직 남겨져 있다
-    private final Collection stampls = new ArrayList();
+    // parameterized type으로 정의해서 타입 안정성을 확보.
+    private final Collection<Stamp> stamps = new ArrayList<>();
 
     public static void main(String[] args) {
         Test test = new Test();
 
-        // Stamp 대신 Coin을 넣어도 오류없이 컴파일 되고 실행 된다
-        // 추후에 런타임 시에 발견된다.
-        test.stampls.add(new Coin());
+        test.stamps.add(new Stamp());
 
-        for(Iterator i = test.stampls.iterator() ; i.hasNext() ;){
-            Stamp stamp = (Stamp) i.next(); // Coin 클래스에 의해 ClassCastException을 던진다.
+        for (Stamp stamp : test.stamps) {
             stamp.cancel();
         }
     }
