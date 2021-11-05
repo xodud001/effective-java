56a3507029487a9957cac9eabd2cab0c763a2e75
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-05 22:29:11 +0900

    feat : item 28 배열보다는 리스트를 사용하라
    제네릭으로 구현 되었지만 형변환 중에 안전을 보장받지 못함

--- src/chapter5/item28/Chooser.java
+++ src/chapter5/item28/Chooser.java
@@ -4,14 +4,15 @@ import java.util.Collection;
 import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-// 제네릭을 쓰지 않고 구현한 클래스
-// choose 메소드를 호출 할 때마다 반환된 Object를 원하는 타입으로 형변환 해야한다.
-// 혹시 타입이 다른 원소가 들어 있다면 런타임에 형변환 오류가 날 것이다.
-public class Chooser {
-    private final Object[] choiceArray;
 
-    public Chooser(Collection choices){
-        choiceArray = choices.toArray();
+// 제네릭을 사용해서 구현한 클래스
+public class Chooser<T> {
+    private final T[] choiceArray;
+
+    public Chooser(Collection<T> choices){
+        // T가 무슨 타입인지 알 수 없으니 컴파일러가 형변환이 런타임에 안전하는 것이 보장되지 않는다는 경고
+        // 동작하지만 안전을 보장하지 못할 뿐
+        choiceArray = (T[]) choices.toArray();
     }
 
     public Object choose(){
