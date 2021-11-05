81d14405f98879b96eb170ea4f9ee40e3a5105a1
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-05 22:32:58 +0900

    feat : item 28 배열보다는 리스트를 사용하라
    배열을 리스트로 바꿔 경고를 제거

--- src/chapter5/item28/Chooser.java
+++ src/chapter5/item28/Chooser.java
@@ -1,22 +1,22 @@
 package chapter5.item28;
 
-import java.util.Collection;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 // 제네릭을 사용해서 구현한 클래스
+// 코드의 양이 조금 늘었고 조금 더 느릴 수 있지만
+// 런타입에 ClassCastExceptoin을 만날 일은 없으니 그만한 가치가 있다
 public class Chooser<T> {
-    private final T[] choiceArray;
+    // 배열을 리스트로 수정
+    private final List<T> choiceList;
 
     public Chooser(Collection<T> choices){
-        // T가 무슨 타입인지 알 수 없으니 컴파일러가 형변환이 런타임에 안전하는 것이 보장되지 않는다는 경고
-        // 동작하지만 안전을 보장하지 못할 뿐
-        choiceArray = (T[]) choices.toArray();
+        // 배열을 리스트로 변환해서 경고를 제거
+        choiceList = new ArrayList<>(choices);
     }
 
     public Object choose(){
         Random rnd = ThreadLocalRandom.current();
-        return choiceArray[rnd.nextInt(choiceArray.length)];
+        return choiceList.get(rnd.nextInt(choiceList.size()));
     }
 }
