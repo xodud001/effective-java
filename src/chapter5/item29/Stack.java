d16fec6650aca32b358be09c4fbfd7d725cca431
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-06 23:29:37 +0900

    feat : item 29 이왕이면 제네릭 타입으로 만들라
    제네릭 배열을 만들지 않고 반환 할 때 제네릭으로 변환해서 반환하는 방법

--- src/chapter5/item29/Stack.java
+++ src/chapter5/item29/Stack.java
@@ -5,18 +5,12 @@ import java.util.EmptyStackException;
 
 // 아이템 7에서 다룬 Stack을 제네릭으로 변경
 public class Stack<E> {
-    private E[] elements;
+    private Object[] elements;
     private int size = 0;
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
 
-    // 하지만 여기서 컴파일 되지 않는다.
-    // 실체화 불가 타입으로는 배열을 만들 수 없다.
-    // 제약을 대놓고 우회해서 제네릭 배열 생성
-    // 컴파일러는 오류 대신 경고를 표시함
-    // 어노테이션을 이용해 경고를 무시하도록 설정
-    @SuppressWarnings("unchecked")
     public Stack(){
-        elements = (E[]) new Object[DEFAULT_INITIAL_CAPACITY];
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
     }
 
     public void push(E e){
@@ -26,7 +20,9 @@ public class Stack<E> {
     public E pop(){
         if (size == 0)
             throw new EmptyStackException();
-        E result = elements[--size];
+        // 반환 할 때 제네릭으로 변경하는 방법
+        @SuppressWarnings("unchecked")
+        E result = (E) elements[--size];
         elements[size] = null;
         return result;
 
