d6d09f5cdc2d5a82ed454fa271b0e49c672197a6
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-07 22:46:00 +0900

    feat : item 30 이왕이면 제네릭 메서드로 만들라
    타입 매개변수를 명시해서 제네릭 타입으로 수정

--- src/chapter5/item30/Test.java
+++ src/chapter5/item30/Test.java
@@ -4,9 +4,17 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Test {
-    public static Set union(Set s1, Set s2){
-        Set result = new HashSet(s1);
+    // 타입 매개변수를 명시해서 제네릭 메소드로 수정
+    public static <E> Set<E> union(Set<E> s1, Set<E> s2){
+        Set<E> result = new HashSet<>(s1);
         result.addAll(s2);
         return result;
+    }
+
+    public static void main(String[] args) {
+        Set<String> one = Set.of("문", "자", "열");
+        Set<String> two = Set.of("입", "니", "다");
+        Set<String> result = union(one, two);
+        System.out.println(result);
     }
 }
