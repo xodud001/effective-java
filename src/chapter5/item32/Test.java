a47b25c10ba1b711cf3d4c97891a5fd4c31b8e50
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-09 21:41:26 +0900

    feat : item 32 제네릭과 가변인수를 함께 쓸 때는 신중하라
    @SafeVarargs 어노테이션을 사용하지 않고 배열 없이 제네릭만 사용해서 타입 안전하게 만듬

--- src/chapter5/item32/Test.java
+++ src/chapter5/item32/Test.java
@@ -1,23 +1,20 @@
 package chapter5.item32;
 
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Test {
     public static void main(String[] args) {
-        // java.lang.Object가 java.lang.String로 변환될 수 없기 때문에 ClassCastExceptoin 발생
-        String[] attributes = pickTwo("좋은", "빠른", "저렴한");
+        // java.lang.Object[]가 java.lang.String[]로 변환될 수 없기 때문에 ClassCastExceptoin 발생
+        List<String> attributes = pickTwo("좋은", "빠른", "저렴한");
     }
 
-    // 제네릭 가변
-    public static <T> T[] toArray(T... args){
-        return args;
-    }
 
-    public static <T> T[] pickTwo(T a, T b, T c){
+    public static <T> List<T> pickTwo(T a, T b, T c){
         switch (ThreadLocalRandom.current().nextInt(3)){
-            case 0: return toArray(a, b);
-            case 1: return toArray(a, c);
-            case 2: return toArray(c, b);
+            case 0: return List.of(a, b);
+            case 1: return List.of(a, c);
+            case 2: return List.of(c, b);
         }
         throw new AssertionError();
     }
