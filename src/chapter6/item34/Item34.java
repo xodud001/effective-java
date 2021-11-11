4bd0c9d36e8965ad2a935eb88c809883d5332f90
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-11 21:37:59 +0900

    feat : item 34 int 상수 대신 열거 타입을 사용하라
    간단한 열거타입. 열거타입을 사용하면 타입이 다른 열거타입끼리 섞일 수 없다

--- src/chapter6/item34/Item34.java
+++ src/chapter6/item34/Item34.java
@@ -1,16 +1,11 @@
 package chapter6.item34;
 
 public class Item34 {
-    public static final int APPLE_FUJI = 0;
-    public static final int APPLE_PIPPIN = 1;
-    public static final int APPLE_GRANNY_SMITH = 2;
-
-    public static final int ORANGE_NAVEL = 0;
-    public static final int ORANGE_TEMPLE = 0;
-    public static final int ORANGE_BLOOD = 0;
+    public enum Apple{ FUJI, PIPPIN, GRANNY_SMITH}
+    public enum Orange{ NAVEL, TEMPLE, BLOOD}
 
     public static void main(String[] args) {
-        // APPLE과 ORANGE를 비교해도 아무런 경고가 없음
-        int i = (APPLE_FUJI - ORANGE_TEMPLE) / APPLE_PIPPIN;
+        // Apple에 Orange를 넣을 수 없음
+        Apple apple = Orange.NAVEL;
     }
 }
