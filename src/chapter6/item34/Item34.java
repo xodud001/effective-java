b4d9472e8242c03d5bca81c7b45f13c46402a891
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-12 23:34:06 +0900

    feat : item 34 int 상수 대신 열거 타입을 사용하라

--- src/chapter6/item34/Item34.java
+++ src/chapter6/item34/Item34.java
@@ -5,7 +5,7 @@ public class Item34 {
     public enum Orange{ NAVEL, TEMPLE, BLOOD}
 
     public static void main(String[] args) {
-        // Apple에 Orange를 넣을 수 없음
-        Apple apple = Orange.NAVEL;
+        // Apple에 Orange를 넣을 수 없음음
+       Apple apple = Orange.NAVEL;
     }
 }
