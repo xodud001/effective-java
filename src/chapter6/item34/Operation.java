3337dbe9d26c3bc8cbc51323c5e4989da2577346
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-13 12:51:00 +0900

    feat : item 34 int 상수 대신 열거 타입을 사용하라
    상수별 메서드 구현을 활용

--- src/chapter6/item34/Operation.java
+++ src/chapter6/item34/Operation.java
@@ -1,25 +1,28 @@
 package chapter6.item34;
 
+// 상수별 메소드 구현을 활용
 public enum Operation {
-    PLUS,
-    MINUS,
-    TIMES,
-    DIVIDE;
-
-    // 새로운 상수를 추가하면 아래 switch 문에 case도 추가해야 됨
-    // 깜빡 한다면 에러가 남
-    public double apply(double x, double y){
-        switch (this){
-            case PLUS:
-                return x + y;
-            case MINUS:
-                return x - y;
-            case TIMES:
-                return x * y;
-            case DIVIDE:
-                return x / y;
+    PLUS{
+        public double apply(double x, double y) {
+            return x + y;
         }
-        // 실제로는 도달 할 수 없지만 기술적으로 도달 가능
-        throw new AssertionError("알 수 없는 연산 : " + this);
-    }
+    },
+    MINUS{
+        public double apply(double x, double y) {
+            return x - y;
+        }
+    },
+    TIMES{
+        public double apply(double x, double y) {
+            return x * y;
+        }
+    },
+    DIVIDE{
+        public double apply(double x, double y) {
+            return x / y;
+        }
+    };
+
+    // 상수별 함수를 정의하기 위한 추상 메서드
+    public abstract double apply(double x, double y);
 }
