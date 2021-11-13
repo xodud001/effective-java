fbd92b9006f67e182e89de7520efb26ecbcdbc7c
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-14 00:02:01 +0900

    feat : item 35 ordinal 메서드 대신 인스턴스 필드를 사용하라
    ordinal 메소드에 대한 의존성을 디커플링해서 유지보수에 좋도록 수정

--- src/chapter6/item35/Ensemble.java
+++ src/chapter6/item35/Ensemble.java
@@ -1,22 +1,24 @@
 package chapter6.item35;
 
-// 동작은 하지만 유지보수가 좋지 못한 코드. 상수 순서에 메소드가 의존적임
-// 8중주(OCTET) 상수가 이미 있어, 똑같이 8명이 연주하는 복4중주(double quartet)는 추가 할 수 없다.
-// 12명이 연주하는 3중 4중주를 추가하려면 11명으로 구성된 연주를 일컫는 말이 없어 더미 상수를 추가해야 한다.
+// 열거 타입 상수에 연결된 값은 ordinal 메소들 얻지 말고, 인스턴스 필드에 저장하자
 public enum Ensemble {
-    SOLO,
-    DUET,
-    TRIO,
-    QUARTET,
-    QUINTET,
-    SEXTET,
-    SEPTET,
-    OCTET,
-    NONET,
-    DECTET;
+    SOLO(1),
+    DUET(2),
+    TRIO(3),
+    QUARTET(4),
+    QUINTET(5),
+    SEXTET(6),
+    SEPTET(7),
+    OCTET(8),
+    NONET(9),
+    DECTET(10),
+    TRIPLE_QUARTET(12);
 
+    private final int numberOfMusicians;
+    Ensemble(int size){
+        this.numberOfMusicians = size;
+    }
     public int numberOfMusicians(){
-        return  ordinal() + 1;
+        return  numberOfMusicians;
     }
 }
-
