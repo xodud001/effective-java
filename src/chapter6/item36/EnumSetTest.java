3f14751bc9423065029e0854be2151151c3bf73d
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-14 20:20:14 +0900

    feat : item 36 비트 필드 대신 EnumSet을 사용하라
    비트 필드 열거 상수를 이용해 집합을 구현

---
+++ src/chapter6/item36/EnumSetTest.java
@@ -0,0 +1,12 @@
+package chapter6.item36;
+
+public class EnumSetTest {
+    public static final int STYLE_BOLD = 1 << 0;
+    public static final int STYLE_ITALIC = 1 << 1;
+    public static final int STYLE_UNDERLINE = 1 << 2;
+    public static final int STYLE_STRIKETHROUGH = 1 << 3;
+
+    public void applyStyles(int styles){
+        
+    }
+}
