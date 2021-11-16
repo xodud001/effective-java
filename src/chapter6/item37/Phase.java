1a82ae0c244cb29739ab55e640d089e6096a18ea
김태영 <51165061+xodud001@users.noreply.github.com>
2021-11-16 22:57:12 +0900

    feat : item 37 ordinal 인덱싱 대신 EnumMap을 사용하라
    EnumMap을 이용해서 수정

--- src/chapter6/item37/Phase.java
+++ src/chapter6/item37/Phase.java
@@ -1,23 +1,42 @@
 package chapter6.item37;
 
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
+
 public enum Phase {
     SOLID,
     LIQUID,
     GAS;
 
     public enum Transition{
-        MELT, FREEZE, BOIL, CONDENSE, SUBLIME, DEPOSIT;
+        MELT(SOLID, LIQUID),
+        FREEZE(LIQUID, SOLID),
+        BOIL(LIQUID, GAS),
+        CONDENSE(GAS, LIQUID),
+        SUBLIME(SOLID, GAS),
+        DEPOSIT(GAS, SOLID);
 
-        private static final Transition[][] TRANSITIONS = {
-                {null, MELT, SUBLIME},
-                { FREEZE, null, BOIL},
-                { DEPOSIT, CONDENSE, null}
-        };
+        private final Phase from;
+        private final Phase to;
+
+        Transition(Phase from, Phase to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        // EnumMap을 이용해서 수정
+        private static final Map<Phase, Map<Phase, Transition>> m =
+                Stream.of(values()).collect(groupingBy(t -> t.from,
+                        () -> new EnumMap<>(Phase.class),
+                        toMap(t -> t.to, t -> t,
+                                (x, y) -> y, () -> new EnumMap<>(Phase.class))));
 
         public static Transition from(Phase from, Phase to){
-            //배열들의 인덱스에 ordinal()을 사용했다.
-            // 컴파일러가 ordinal과 배열 인덱스의 관계를 알 수 없으므로 위험하다.
-            return TRANSITIONS[from.ordinal()][to.ordinal()];
+            return m.get(from).get(to);
         }
     }
 }
