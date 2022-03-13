84e842ebec6f3c0fd306f12398b8554729b4ad38
Kim Taeyeong <freeptk1@gmail.com>
2022-03-13 22:24:26 +0900

    feat : 스트림을 제대로 활용한 코드

--- src/chapter7/item46/Item46.java
+++ src/chapter7/item46/Item46.java
@@ -8,17 +8,19 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 public class Item46 {
 
 
     public static void main(String[] args) throws FileNotFoundException {
         InputStream file = new FileInputStream("dst.txt");
-        // 스트림 패러다임을 이해하지 못한 채 API만 사용
-        Map<String, Long> freq = new HashMap<>();
-        try(Stream<String> words = new Scanner(file).tokens()){
-            words.forEach(word -> {
-                freq.merge(word.toLowerCase(), 1L, Long::sum);
-            });
+        // 스트림을 제대로 활용한 코드
+        Map<String, Long> freq;
+        try (Stream<String> words = new Scanner(file).tokens()) {
+            freq = words
+                    .collect(groupingBy(String::toLowerCase, counting()));
         }
     }
 }
