8a209f3aff04043a9dce200491e71d09d4e50214
Kim Taeyeong <freeptk1@gmail.com>
2022-03-14 23:48:15 +0900

    feat : 형변환으로 자바 타입 추론 문제 해결

--- src/chapter7/item47/Item47.java
+++ src/chapter7/item47/Item47.java
@@ -3,8 +3,8 @@ package chapter7.item47;
 public class Item47 {
 
     public static void main(String[] args) {
-        // 자바 타입 추론의 한계로 컴파일 되지 않음
-        for (ProcessHandle ph : ProcessHandle.allProcesses()::iterator){
+        // 동작하지만 안 좋은 우회방법
+        for (ProcessHandle ph : (Iterable<ProcessHandle>)ProcessHandle.allProcesses()::iterator){
 
         }
     }
