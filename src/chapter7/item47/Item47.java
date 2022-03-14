642dd82f29a093ebcfaea386f3cf91cb86dd3997
Kim Taeyeong <freeptk1@gmail.com>
2022-03-14 23:49:49 +0900

    feat : 어댑터로 해결

--- src/chapter7/item47/Item47.java
+++ src/chapter7/item47/Item47.java
@@ -1,11 +1,17 @@
 package chapter7.item47;
 
+import java.util.stream.Stream;
+
 public class Item47 {
 
     public static void main(String[] args) {
-        // 동작하지만 안 좋은 우회방법
-        for (ProcessHandle ph : (Iterable<ProcessHandle>)ProcessHandle.allProcesses()::iterator){
+        // 어댑터로 해결
+        for (ProcessHandle ph : iterableOf(ProcessHandle.allProcesses())){
 
         }
+    }
+
+    public static <E> Iterable<E> iterableOf(Stream<E> stream){
+        return stream::iterator;
     }
 }
