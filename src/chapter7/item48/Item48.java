248400444755128df36c65a71f4935cce2f771e1
Kim Taeyeong <freeptk1@gmail.com>
2022-03-15 23:52:43 +0900

    feat : parallel()로 병렬로 계산하기

--- src/chapter7/item48/Item48.java
+++ src/chapter7/item48/Item48.java
@@ -1,6 +1,7 @@
 package chapter7.item48;
 
 import java.math.BigInteger;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 import static java.math.BigInteger.*;
@@ -9,13 +10,18 @@ public class Item48 {
 
     // 스트림으로 처음 20개의 메르센 소수를 생성
     public static void main(String[] args) {
-        primes().map(p -> TWO.pow(p.intValueExact()).subtract(ONE))
-                .filter(mersenne -> mersenne.isProbablePrime(50))
-                .limit(20)
-                .forEach(System.out::println);
+        long start = System.currentTimeMillis();
+        System.out.println(pi(2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2));
+        System.out.println(System.currentTimeMillis() - start);
     }
 
-    static Stream<BigInteger> primes(){
-        return Stream.iterate(TWO, BigInteger::nextProbablePrime);
+    private static long pi(long n) {
+        return LongStream.rangeClosed(2, n)
+                .parallel()
+                .mapToObj(BigInteger::valueOf)
+                .filter(i -> i.isProbablePrime(50))
+
+                .count();
     }
+
 }
