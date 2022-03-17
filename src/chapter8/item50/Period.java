c2654fa0f521541d85d53fc577276c70f7e23f75
Kim Taeyeong <freeptk1@gmail.com>
2022-03-17 23:28:03 +0900

    feat : 방어적 복사를 이용해 취약점 해결

--- src/chapter8/item50/Period.java
+++ src/chapter8/item50/Period.java
@@ -7,16 +7,17 @@ public class Period {
     private final Date start;
     private final Date end;
 
+    // 방어적 복사를 이용해서 원본을 노출 시키지 않아 변경을 막기
     public Period(Date start, Date end) {
-        this.start = start;
-        this.end = end;
+        this.start = new Date(start.getTime());
+        this.end = new Date(end.getTime());
     }
 
     public Date getStart() {
-        return start;
+        return new Date(start.getTime());
     }
 
     public Date getEnd() {
-        return end;
+        return new Date(end.getTime());
     }
 }
