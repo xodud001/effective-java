e4d27ea7d7f413024050dd87e2eddd762d3adbc5
Kim Taeyeong <freeptk1@gmail.com>
2022-03-18 23:46:54 +0900

    feat : 잘못된 다중정의로 예측하기 힘든 코드

---
+++ src/chapter8/item52/CollectionClassifier.java
@@ -0,0 +1,31 @@
+package chapter8.item52;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class CollectionClassifier {
+
+    public static String classify(Set<?> s){
+        return "집합";
+    }
+
+    public static String classify(List<?> list) {
+        return "리스트";
+    }
+
+    public static String classify(Collection<?> c) {
+        return "그 외";
+    }
+
+    public static void main(String[] args) {
+        Collection<?>[] collections = {
+                new HashSet<String>(),
+                new ArrayList<BigInteger>(),
+                new HashMap<String, String >().values()
+        };
+
+        for (Collection<?> collection : collections) {
+            System.out.println(classify(collection));
+        }
+    }
+}
