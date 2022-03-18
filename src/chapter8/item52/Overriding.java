191b616f2826a0798adae09918e2811462304bbb
Kim Taeyeong <freeptk1@gmail.com>
2022-03-19 01:04:26 +0900

    feat : 오버로딩으로 예측 가능하게 하기

---
+++ src/chapter8/item52/Overriding.java
@@ -0,0 +1,33 @@
+package chapter8.item52;
+
+import java.awt.image.WritableRenderedImage;
+import java.util.List;
+
+class Wine {
+    String name() { return "포도주";}
+}
+
+class SparklingWine extends Wine{
+    @Override
+    String name() {
+        return "발포성 포도주";
+    }
+}
+
+class Champagne extends Wine{
+    @Override
+    String name() {
+        return "샴페인";
+    }
+}
+
+public class Overriding{
+    public static void main(String[] args) {
+        List<Wine> wines = List.of(
+                new Wine(), new SparklingWine(), new Champagne());
+
+        for (Wine wine : wines) {
+            System.out.println(wine.name());
+        }
+    }
+}
