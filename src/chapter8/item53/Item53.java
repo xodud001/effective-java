d6b47163edeff205185e5283adfbfaf669140e61
Kim Taeyeong <freeptk1@gmail.com>
2022-03-20 22:54:19 +0900

    Item53 - 가변인수를 잘 사용한 예

--- src/chapter8/item53/Item53.java
+++ src/chapter8/item53/Item53.java
@@ -2,16 +2,13 @@ package chapter8.item53;
 
 public class Item53 {
 
-    // 인수가 1개 이상이어야 하는 가변인수 메서드 - 잘못 구현한 예
-    static int min(int... args){
-        if (args.length == 0)
-            throw new IllegalArgumentException("인수가 1개 이상 필요합니다.");
-
-        int min = args[0];
-        for (int i = 1; i < args.length; i++)
-            if (args[i] < min)
-                min = args[i];
-        return sum;
+    // 인수가 1개 이상이어야 할 때 가변인수를 제대로 사용하는 방법
+     static int min(int firstArg, int... remainingArgs){
+        int min = firstArg;
+        for (int arg : remainingArgs)
+            if(arg < min)
+                min = arg;
+        return min;
     }
 
 }
