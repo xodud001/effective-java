43d870bfbfb62c60ecc0016c4e3d96e9f995078b
Kim Taeyeong <freeptk1@gmail.com>
2022-03-21 00:12:27 +0900

    Item54 - null을 반환할 경우 클라이언트에서 확인하는 코드를 작성해야 됨

--- src/chapter8/item54/Item54.java
+++ src/chapter8/item54/Item54.java
@@ -8,10 +8,13 @@ public class Item54 {
     static List<Cheese> cheesesInStock = new ArrayList<>();
 
     public static void main(String[] args) {
+        List<Cheese> cheeses = getCheeses();
+        if (cheeses != null && cheeses.contains(new Cheese()))
+            System.out.println("That's right!");
 
     }
 
-    public List<Cheese> getCheeses(){
+    public static List<Cheese> getCheeses(){
         // 컬렉션이 비었으면 null을 반환
         return cheesesInStock.isEmpty() ? null
                 : new ArrayList<>(cheesesInStock);
