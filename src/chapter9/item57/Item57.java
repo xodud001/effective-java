5ae834a863d717549592e7c8130b832822e22e17
Kim Taeyeong <freeptk1@gmail.com>
2022-03-22 23:39:40 +0900

    Item57 : for문을 이용해 반복하면 복사 붙여넣기로 인한 실수를 막을 수 있다

--- src/chapter9/item57/Item57.java
+++ src/chapter9/item57/Item57.java
@@ -10,15 +10,10 @@ public class Item57 {
     public static void main(String[] args) {
         List<Element> c = new ArrayList<>();
 
-        // 지역변수의 범위가 커져서 i와 i2를 2개 선언 했지만 복사 붙여넣기의 실수로
-        // i만 사용되는 실수를 범할 수 있다.
-        Iterator<Element> i = c.iterator();
-        while (i.hasNext()) {
+        for(Iterator<Element> i = c.iterator(); i.hasNext(); ){
             Element next = i.next();
         }
-
-        Iterator<Element> i2 = c.iterator();
-        while (i.hasNext()) {
+        for(Iterator<Element> i2 = c.iterator(); i.hasNext(); ){
             Element next = i.next();
         }
 
