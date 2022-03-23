07a8dc3fb9e2baec26b24a9ddac8b5e126afdf56
Kim Taeyeong <freeptk1@gmail.com>
2022-03-23 23:39:40 +0900

    Item60 - 소수점 자체를 안쓰도록 변경

--- src/chapter9/item60/Item60.java
+++ src/chapter9/item60/Item60.java
@@ -6,20 +6,14 @@ import java.math.BigInteger;
 public class Item60 {
 
     public static void main(String[] args) {
-        final BigDecimal TEN_CENTS = new BigDecimal(".10");
-
         int itemsBought = 0;
-        BigDecimal funds = new BigDecimal("1.00"); // 1달러
+        int funds = 100; // 100센트
 
         // 물건을 10센트, 20센트, 30센트.... 순으로 구매
-        for (BigDecimal price = TEN_CENTS;
-                funds.compareTo(price) >= 0;
-                price = price.add(TEN_CENTS)) {
-            funds = funds.subtract(price);
+        for (int price = 10; funds >= price; price += 10) {
+            funds -= price;
             itemsBought++;
         }
-        // 10센트, 20센트, 30센트, 40센트면 1달러기 때문에
-        // 4개가 구매되어야 하지만 실제론 3개가 구매된다.
         System.out.println(itemsBought + "개 구입");
         System.out.println("잔돈(달러) = " + funds);
     }
