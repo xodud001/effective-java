4ad56c1d4e3d4ee5b7d3fe6a194fe52d6dae92d4
Kim Taeyeong <freeptk1@gmail.com>
2022-03-23 23:31:32 +0900

    Item60 - double 형으로 금융 계산을 했을 때 생기는 문제

--- src/chapter9/item60/Item60.java
+++ src/chapter9/item60/Item60.java
@@ -3,10 +3,16 @@ package chapter9.item60;
 public class Item60 {
 
     public static void main(String[] args) {
-        // 실수 계산은 정확한 답을 구하는 곳에서는 피해야 한다.
-        System.out.println(1.03 - 0.42);
-        // 0.6100000000000001
-        System.out.println(1.00 - 9 * 0.10);
-        // 0.09999999999999998
+        double funds = 1.00; // 1달러
+        int itemsBought = 0;
+        // 물건을 10센트, 20센트, 30센트.... 순으로 구매
+        for (double price = 0.10; funds >= price; price += 0.10) {
+            funds -= price;
+            itemsBought++;
+        }
+        // 10센트, 20센트, 30센트, 40센트면 1달러기 때문에
+        // 4개가 구매되어야 하지만 실제론 3개가 구매된다.
+        System.out.println(itemsBought + "개 구입");
+        System.out.println("잔돈(달러) = " + funds);
     }
 }
